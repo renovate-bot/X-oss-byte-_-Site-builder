a454cb2d93e032fab91db9321d843ae70a35d067
olawanlejoel <info4tobest2@gmail.com>
2023-07-02 17:32:05 +0100

    first commit

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,35 @@
+import { useState } from 'react';
+
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 import { FiExternalLink } from "react-icons/fi";
 
 const Details = () => {
+    const [operationData, setOperationData] = useState({ message: "Operation in progress" });
+
+    const KinstaAPIUrl = 'https://api.kinsta.com/v2';
+    const stateData = JSON.parse(localStorage.getItem('state'));
+
+    const checkOperation = async () => {
+        const operationId = stateData.operationId;
+        const resp = await fetch(
+            `${KinstaAPIUrl}/operations/${operationId}`,
+            {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${process.env.REACT_APP_KINSTA_API_KEY}`
+                }
+            }
+        );
+
+        const data = await resp.json();
+        setOperationData({ ...data, site_name: stateData.site_name });
+    }
+
+    if (stateData) {
+        setTimeout(checkOperation, 5 * 60 * 1000);
+    }
 
     return (
         <div className="app-container">
@@ -17,19 +43,19 @@ const Details = () => {
                 </div>
                 <div className="services">
                     <div className="details">
-                        <p>Operation status..</p>
-                        <button className='sm-btn'>Check Site Status</button>
+                        <p>{operationData.message}..</p>
+                        <button className='sm-btn' onClick={() => checkOperation()}>Check Site Status</button>
                     </div>
                 </div>
 
                 <div className="services">
                     <p className="description">If message above indicates that "Operation has successfully finished", use the links below to access your WP admin and the site itself.</p>
                     <div className="details">
-                        <a href={`http://siteName.kinsta.cloud/wp-admin/`} target="_blank" rel="noreferrer" className='detail-link'>
+                        <a href={`http://${stateData.site_name}.kinsta.cloud/wp-admin/`} target="_blank" rel="noreferrer" className='detail-link'>
                             <p>Open WordPress admin</p>
                             <FiExternalLink />
                         </a>
-                        <a href={`http://siteName.kinsta.cloud/`} target="_blank" rel="noreferrer" className='detail-link'>
+                        <a href={`http://${stateData.site_name}.kinsta.cloud/`} target="_blank" rel="noreferrer" className='detail-link'>
                             <p>Open URL</p>
                             <FiExternalLink />
                         </a>
